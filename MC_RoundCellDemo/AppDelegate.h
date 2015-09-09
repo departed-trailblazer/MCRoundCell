a5274910f7a89547f5c5908407caabf25e5c991e
departed-trailblazer <402178719@qq.com>
2015-09-09 16:54:19 +0800

    Initial Commit

---
+++ MC_RoundCellDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MC_RoundCellDemo
+//
+//  Created by xulu on 15/9/9.
+//  Copyright (c) 2015年 _MC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
